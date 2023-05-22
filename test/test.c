faa8ea63c344465b3694b7f1fc6fb9416c935ec6
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2023-05-22 18:38:39 +0530

    added test programs

---
+++ test/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    printf("Enter two numbers: ");
+    scanf(" %d %d", &num1, &num2);
+
+    printf("sum: %d", num1+num2);
+
+    return 0;
+}
---
+++ test/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    cout<<"Enter two numbers: ";
+
+    cin>>num1>>num2;
+
+    cout<<"Sum: "<<num1+num2;
+    return 0;
+}
